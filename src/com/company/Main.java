f1ec742fc5fb0445461848e8333f26fbd4a12194
Нурланбековдастан <nurlanbekovdastan623@gmail.com>
2021-06-12 15:31:13 +0600

    do it dz

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefenceType = " ";
+    public static int[] heroesHealth = {250,250,250};
+    public static int[] heroesDamage = {25,23,32};
+    public static String[] heroesAttackType = {"Physical","Magical","Mental"};
+    public static int  doctoreHealth = 500;
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
